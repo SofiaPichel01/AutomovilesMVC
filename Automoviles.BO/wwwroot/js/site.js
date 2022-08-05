c4fd2a8f007e2a61afbbdcd7b09d0e3cebf41beb
Sofia Lara Pichel <spichel@smsargentina.com.ar>
2022-08-05 17:50:17 -0300

    Cambios en el home/Index(cards), cambios en estilo crud de vehículos

--- Automoviles.BO/wwwroot/js/site.js
+++ Automoviles.BO/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+//JS GRID
+$(document).ready(function () {
+    $('#table_id').DataTable();
+});
