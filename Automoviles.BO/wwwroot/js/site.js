e4c556aae9cd80773fd394e6ce74e403f63cc7b4
Sofia Lara Pichel <spichel@smsargentina.com.ar>
2022-08-08 16:04:38 -0300

    Tabla jquery agregada

--- Automoviles.BO/wwwroot/js/site.js
+++ Automoviles.BO/wwwroot/js/site.js
@@ -3,6 +3,36 @@
 
 // Write your JavaScript code.
 //JS GRID
+
 $(document).ready(function () {
-    $('#table_id').DataTable();
+    $('table').DataTable();
 });
+
+$("table").DataTable({
+    "language": {
+        "sProcessing": "Procesando...",
+        "sLengthMenu": "Mostrar _MENU_ registros",
+        "sZeroRecords": "No se encontraron resultados",
+        "sEmptyTable": "Ningún dato disponible en esta tabla",
+        "sInfo": "Mostrando  _END_ / _TOTAL_ registros",
+        "sInfoEmpty": "Sin datos",
+        "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+        "sInfoPostFix": "",
+        "sSearch": "Buscar:",
+        "sUrl": "",
+        "sInfoThousands": ",",
+        "sLoadingRecords": "Cargando...",
+        "oPaginate": {
+            "sFirst": "Primero",
+            "sLast": "Último",
+            "sNext": "Siguiente",
+            "sPrevious": "Anterior"
+        },
+        "oAria": {
+            "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+        },
+    },
+    "pagingType": "numbers"
+});
+
